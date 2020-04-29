536e6bbcb6f7bb6fed6c8b2c1143b9470fef5c5e
ayush <ayushsahu99@gmail.com>
2020-04-29 19:00:47 +0530

    Initialized parameters

---
+++ parameter_initialize.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 18:47:45 2020
+
+@author: Dell
+"""
+
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def par_init():
+    # Importing the dataset
+    dataset = pd.read_csv('Churn_Modelling.csv')
+    X = dataset.iloc[:, 3:13].values
+    y = dataset.iloc[:, 13].values
+    
+    # Encoding categorical data
+    from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+    labelencoder_X_1 = LabelEncoder()
+    X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+    labelencoder_X_2 = LabelEncoder()
+    X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+    onehotencoder = OneHotEncoder(categorical_features = [1])
+    X = onehotencoder.fit_transform(X).toarray()
+    X = X[:, 1:]
+    
+    # Splitting the dataset into the Training set and Test set
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+    
+    # Feature Scaling
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+    y_train = y_train.reshape(-1, 1)
+    y_test = y_test.reshape(-1, 1)
+    X_train, y_train, X_test, y_test = X_train.T, y_train.T, X_test.T, y_test.T
+    assert(X_train.shape == (11, 8000))
+    assert(X_test.shape == (11, 2000))
+    assert(y_train.shape == (1, 8000))
+    assert(y_test.shape == (1, 2000))
+    return X_train, y_train, X_test, y_test
+
+#X_train, y_train, X_test, y_test = par_init()
