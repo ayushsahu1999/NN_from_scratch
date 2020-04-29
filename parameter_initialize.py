b51e085c1873dd20048038ac1d4851f8d4b9964f
ayush <ayushsahu99@gmail.com>
2020-04-29 19:21:37 +0530

    Parameters Initialize

--- parameter_initialize.py
+++ parameter_initialize.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 29 18:47:45 2020
+Created on Wed Apr 29 19:04:25 2020
 
 @author: Dell
 """
@@ -10,38 +10,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def par_init():
-    # Importing the dataset
-    dataset = pd.read_csv('Churn_Modelling.csv')
-    X = dataset.iloc[:, 3:13].values
-    y = dataset.iloc[:, 13].values
-    
-    # Encoding categorical data
-    from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-    labelencoder_X_1 = LabelEncoder()
-    X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-    labelencoder_X_2 = LabelEncoder()
-    X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-    onehotencoder = OneHotEncoder(categorical_features = [1])
-    X = onehotencoder.fit_transform(X).toarray()
-    X = X[:, 1:]
-    
-    # Splitting the dataset into the Training set and Test set
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
-    
-    # Feature Scaling
-    from sklearn.preprocessing import StandardScaler
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
-    y_train = y_train.reshape(-1, 1)
-    y_test = y_test.reshape(-1, 1)
-    X_train, y_train, X_test, y_test = X_train.T, y_train.T, X_test.T, y_test.T
-    assert(X_train.shape == (11, 8000))
-    assert(X_test.shape == (11, 2000))
-    assert(y_train.shape == (1, 8000))
-    assert(y_test.shape == (1, 2000))
-    return X_train, y_train, X_test, y_test
 
-#X_train, y_train, X_test, y_test = par_init()
+# Initializing the parameters
+def para_init(layers_dims):
+    from data_init import dat_init
+    X_train, y_train, X_test, y_test = dat_init()
+    parameters = {}
+    L = len(layers_dims)
+    for l in  range(1, L):
+        parameters["W"+str(l)] = np.random.randn(layers_dims[l], layers_dims[l-1]) * 0.01
+        parameters["b"+str(l)] = np.zeros((layers_dims[l], 1))
+        
+        assert(parameters["W"+str(l)].shape == (layers_dims[l], layers_dims[l-1]))
+        assert(parameters["b"+str(l)].shape == (layers_dims[l], 1))
+    return parameters
+
+"""
+layers = [11, 6, 6, 1]
+par = para_init(layers)
+print (par["W1"].shape)
+print (par["b1"].shape)
+print (par["W2"].shape)
+print (par["b2"].shape)
+print (par["W3"].shape)
+print (par["b3"].shape)
+"""
